054d8f916d10ccaac81cc7548b3ae2cf9689c683
miranda-daniel <miranda_daniel@hotmail.com>
2026-04-27 20:45:25 -0300

    refactor(instruments): introduce InstrumentService, controller no longer imports client directly

---
+++ src/services/instrument-service.ts
@@ -0,0 +1,7 @@
+import { InstrumentClient, InstrumentSearchResult } from '../clients/twelve-data-client';
+
+export const InstrumentService = {
+  search: (query: string): Promise<InstrumentSearchResult[]> => InstrumentClient.search(query),
+
+  getQuote: (symbol: string): Promise<number> => InstrumentClient.getQuote(symbol),
+};
