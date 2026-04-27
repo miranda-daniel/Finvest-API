71e10599c0b7e4932cbb1974d9eba68296423e89
miranda-daniel <miranda_daniel@hotmail.com>
2026-04-27 20:40:41 -0300

    feat(errors): add string code field to ErrorInterface, ApiError, and all error constants

--- src/config/api-error.ts
+++ src/config/api-error.ts
@@ -1,16 +1,20 @@
 export interface ErrorInterface {
+  code: string;
   httpCode: number;
   errorCode: number;
   description: string;
 }
 
 export class ApiError extends Error {
+  code;
+
   httpCode;
 
   errorCode;
 
   constructor(error: ErrorInterface) {
     super(error.description);
+    this.code = error.code;
     this.httpCode = error.httpCode;
     this.errorCode = error.errorCode;
   }
